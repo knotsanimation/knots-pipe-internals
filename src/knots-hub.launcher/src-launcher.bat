5d2b0091b6d50c12dbf16d3a242630fe63826223
Liam Collod <lcollod@gmail.com>
2024-10-06 14:37:15 +0200

    refacto(knots-hub.launcher): compatibility with hub 0.8.0

--- src/knots-hub.launcher/src-launcher.bat
+++ src/knots-hub.launcher/src-launcher.bat
@@ -5,21 +5,16 @@ set "rootdir=%~dp0"
 set "buildir=%rootdir%..\builds"
 set "configdir=%rootdir%..\configs"
 
+:: XXX: update those 3 variables to update the knots-hub version used by artists
+set "latestbuild=%buildir%\0.8.0-20241006142747"
+set "exepath=%latestbuild%\knots_hub-v0.8.0.exe"
+set "KNOTSHUB_INSTALLER=0.8.0+20241006142747=%latestbuild%"
+
 set "KNOTSHUB_USER_INSTALL_PATH=%LOCALAPPDATA%\knots-hub"
-set "KNOTSHUB_INSTALLER_LIST=%buildir%\install-list.json"
 set "KNOTSHUB_VENDOR_INSTALLERS_CONFIG=%configdir%\knots-hub.vendor-installers.json"
 :: not actually read by knots-hub but referenced in the installer config
 set "KNOTSHUB_VENDOR_INSTALL_ROOT=%LOCALAPPDATA%\knots-hub.vendors"
 set "KNOTSHUB_VENDOR_REZ_ROOT=%KNOTSHUB_VENDOR_INSTALL_ROOT%\rez"
-
-:: we assume the shortcut link is still defined as "knots-hub"
-:: (managed by knots-hub repository)
-set "exepath=%KNOTSHUB_USER_INSTALL_PATH%\knots-hub.lnk"
-:: if the program was never installed yet the server build will install it and
-:: restart to it.
-if not exist %exepath% (
-   set "exepath=%buildir%\latest\knots-hub.exe"
-)
 
 echo %date% %time% ^| starting %exepath%
 echo (Press any key to exit the prompt once finished)
