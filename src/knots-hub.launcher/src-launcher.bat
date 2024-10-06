8d3678245cb0d7e630edfad84597a1efd0ac511e
Liam Collod <lcollod@gmail.com>
2024-10-06 20:25:40 +0200

    chore(knots-hub.launcher): update hub to 0.8.2

--- src/knots-hub.launcher/src-launcher.bat
+++ src/knots-hub.launcher/src-launcher.bat
@@ -7,9 +7,9 @@ set "buildir=%rootdir%..\builds"
 set "configdir=%rootdir%..\configs"
 
 :: XXX: update those 3 variables to update the knots-hub version used by artists
-set "latestbuild=%buildir%\0.8.1-20241006172141"
-set "KNOTSHUB_SERVER_EXE_PATH=%latestbuild%\knots_hub-v0.8.1.exe"
-set "KNOTSHUB_INSTALLER=0.8.1+20241006172141=%latestbuild%"
+set "latestbuild=%buildir%\0.8.2-20241006202356"
+set "KNOTSHUB_SERVER_EXE_PATH=%latestbuild%\knots_hub-v0.8.2.exe"
+set "KNOTSHUB_INSTALLER=0.8.2+20241006202356=%latestbuild%"
 
 set "KNOTSHUB_USER_INSTALL_PATH=%LOCALAPPDATA%\knots-hub"
 set "KNOTSHUB_VENDOR_INSTALLERS_CONFIG=%configdir%\knots-hub.vendor-installers.json"
