b8e2f33ba7ffb00ffafb61951e3366b7ddf7623f
Liam Collod <lcollod@gmail.com>
2024-10-07 13:05:02 +0200

    fix(knots-hub.launcher): fix vendor config env var
    changed in 0.7.0

--- src/knots-hub.launcher/src-launcher.bat
+++ src/knots-hub.launcher/src-launcher.bat
@@ -12,7 +12,7 @@ set "KNOTSHUB_SERVER_EXE_PATH=%latestbuild%\knots_hub-v0.9.1.exe"
 set "KNOTSHUB_INSTALLER=0.9.1+20241006210225=%latestbuild%"
 
 set "KNOTSHUB_USER_INSTALL_PATH=%LOCALAPPDATA%\knots-hub"
-set "KNOTSHUB_VENDOR_INSTALLERS_CONFIG=%configdir%\knots-hub.vendor-installers.json"
+set "KNOTSHUB_VENDOR_INSTALLER_CONFIG_PATHS=%configdir%\knots-hub.vendor-installers.json"
 :: not actually read by knots-hub but referenced in the installer config
 set "KNOTSHUB_VENDOR_INSTALL_ROOT=%LOCALAPPDATA%\knots-hub.vendors"
 set "KNOTSHUB_VENDOR_REZ_ROOT=%KNOTSHUB_VENDOR_INSTALL_ROOT%\rez"
