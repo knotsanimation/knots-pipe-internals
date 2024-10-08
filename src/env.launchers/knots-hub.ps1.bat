d0fec4bf3145e52829e72f4f7f865088faa9457e
Liam Collod <lcollod@gmail.com>
2024-10-08 16:52:31 +0200

    !refacto(launchers): remove lnk, use batch scripts instead

---
+++ src/env.launchers/knots-hub.ps1.bat
@@ -0,0 +1,3 @@
+:: for powershell command line argument see:
+:: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_powershell_exe
+start "" "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" -NoExit -File "%~dp0.\setup-env.ps1"
