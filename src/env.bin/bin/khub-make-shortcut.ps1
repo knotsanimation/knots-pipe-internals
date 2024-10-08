e64665e7c3c94f5a06a1d39f72b844ac5f879c95
Liam Collod <lcollod@gmail.com>
2024-10-08 17:14:38 +0200

    refacto(env.bin): khub-make-shortcut use changed lnk

--- src/env.bin/bin/khub-make-shortcut.ps1
+++ src/env.bin/bin/khub-make-shortcut.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = "Stop"
 $desktop_path = [Environment]::GetFolderPath([Environment+SpecialFolder]::Desktop)
 $shortcut_path = "${desktop_path}\knots-hub.lnk"
 $icon_path = "${PSScriptRoot}\..\launchers\icon.ico" | Resolve-Path
-$reference_path = "${PSScriptRoot}\..\launchers\knots-hub.${shortcut_type}.bat" | Resolve-Path
+$reference_path = "${PSScriptRoot}\..\launchers\knots-hub.${shortcut_type}.lnk" | Resolve-Path
 
 if (Test-Path $shortcut_path) {
     Remove-Item $shortcut_path -Force
@@ -20,6 +20,7 @@ $wsobject = New-Object -ComObject WScript.Shell;
 $shortcut = $wsobject.CreateShortcut($shortcut_path);
 # https://learn.microsoft.com/en-us/previous-versions/3s9bx7at(v=vs.80)
 $shortcut.IconLocation = "$icon_path";
+# note: a lnk targeting another lnk will just inherit the target lnk path expression
 $shortcut.TargetPath = "$reference_path";
 $shortcut.Save()
 
