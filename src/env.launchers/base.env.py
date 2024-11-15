c4dd45f7e2a4ab2cc7f0c4f7cd012d0e74849e28
Liam Collod <lcollod@gmail.com>
2024-11-15 20:46:56 +0100

    chore(env.launchers): update knots-hub to 0.13.2 build2

--- src/env.launchers/base.env.py
+++ src/env.launchers/base.env.py
@@ -17,14 +17,14 @@ configdir = knotshubdir / "configs"
 
 # those 2 next variables determine which version of knots-hub the artist need to use
 last_build_exe_name = "knots_hub-v0.13.2.exe"
-last_build_name = "0.13.2-20241027113413"
+last_build_name = "0.13.2-20241115201433"
 
 last_build_version = last_build_name.replace("-", "+")
 last_build_path = buildir / last_build_name
 
 ENVIRONMENT = {
     # meta variable to increment everytime this file is modified; for debugging
-    "__KNOTS_ENV_VERSION__": "2",
+    "__KNOTS_ENV_VERSION__": "3",
     # __________________________
     # // knots-hub configuration
     "KNOTSHUB_SERVER_EXE_PATH": str(last_build_path / last_build_exe_name),
